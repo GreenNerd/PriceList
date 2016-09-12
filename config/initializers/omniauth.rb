80960615b3e754f6cae94865de76e3b85a29fc66
dxwang <dxwang68@126.com>
2016-09-12 17:38:50 +0800

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,9 @@ require 'omniauth-wechat-oauth2'
 
 # https://pacific-caverns-73747.herokuapp.com/
 
-WECHAT_APPID = wx2c4e39d960fb0b3b
-WECHAT_APPSECRET = e27e74d17311f9605879e7619d291703
+# WECHAT_APPID = wx2c4e39d960fb0b3b
+# WECHAT_APPSECRET = e27e74d17311f9605879e7619d291703
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :wechat, ENV["WECHAT_APPID"], ENV["WECHAT_APPSECRET"]
+  provider :wechat, ENV["wx2c4e39d960fb0b3b"], ENV["e27e74d17311f9605879e7619d291703"]
 end
