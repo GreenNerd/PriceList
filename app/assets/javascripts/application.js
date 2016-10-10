ed4a664279456e413c2eb6611236db37ae8c456c
suma <dxwang68@gmail.com>
2016-10-11 07:25:04 +0800

    add addreduce.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,21 +14,4 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-//= require_self
 //= require_tree .
-
-function Add(node)
-{
-  var inputs=node.parentNode.getElementById("input");
-  var a=new Number(inputs[1].value);
-  a=a+1;
-  inputs[1].value=""+a;
-}
-function Sub(node)
-{
-  //node.parentNode的类型为单元格
-  var inputs=node.parentNode.getElementById("input");
-  var a=new Number(inputs[1].value);
-  a=a-1;
-  inputs[1].value=inputs[1].value-1;
-}
