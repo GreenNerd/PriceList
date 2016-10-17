c44d3b60a4575470810c5c8bb493e085b253222a
dxwang <dxwang68@126.com>
2016-10-17 09:43:23 +0800

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.page(params[:page])
     if params[:search]
@@ -10,7 +11,29 @@ class ProductsController < ApplicationController
   end
 
   def show
+    set_cartinfo
+  end
+
+  def addtocart
+    @order = current_order
+    @order_item = @order.order_items.new(order_item_params)
+    respond_to do |format|
+      if @order.save
+        flash.now[:notice]="add to cart success"
+      else
+        flash.now[:notice]="add to cart fail"
+      end
+    end
+  end
+
+private
+  def set_cartinfo
     @product = Product.find(params[:id])
     @skus = StockKeepingUnit.where(product_id: params[:id])
   end
+
+  def order_item_params
+    params.require(:order_item).permit(:quntity, :stock_keeping_unit_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,16 @@
 Rails.application.routes.draw do
   get 'search/get'
-
   get 'sessions/new'
 
   get 'signup',      to: 'users#new'
   post 'signup',     to: 'users#create'
-
   get 'login',       to: 'sessions#new'
   post 'login',      to: 'sessions#create'
   delete 'logout',   to: 'sessions#destroy'
-
   get 'search',      to: 'search#get'
+  get'products/addtocart', to: 'products#addtocart'
 
-  #get 'order_items/create'
-  #get 'order_items/update'
-  #get 'order_items/destroy'
-  #get 'carts/show'
-  #get 'products/index'
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show, :addtocart]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy], defaults: { format: 'js' }
   resources :users
