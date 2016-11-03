a314d6bf5f5b2868a550d60d2a256d38dbbc3a0c
dxwang <dxwang68@126.com>
2016-11-03 18:49:57 +0800

    add button add/del increment

--- app/assets/javascripts/detailSel.js
+++ app/assets/javascripts/detailSel.js
@@ -61,3 +61,21 @@ $("*").on("click", ".sb-toggle-submenu", function(){
 		$submenu.slideUp(200);
 	}
 });
+
+// Add(+/1) Button Number Incrementers
+$("*").on("click", ".incr-btn", function(e) {
+  var $button = $(this);
+  var oldValue = $button.parent().find("input").val();
+  if ($button.text() == "+") {
+    var newVal = parseFloat(oldValue) + 1;
+  } else {
+    // Don't allow decrementing below 1
+    if (oldValue > 1) {
+      var newVal = parseFloat(oldValue) - 1;
+    } else {
+      newVal = 1;
+    }
+  }
+  $button.parent().find("input").val(newVal);
+  e.preventDefault();
+});
