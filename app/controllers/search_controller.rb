9452533f75bb234c51d93a6204b949fcf514ad8b
dxwang <dxwang68@126.com>
2016-11-03 15:15:13 +0800

    finish search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,24 +5,10 @@ class SearchController < ApplicationController
       if params[:search].include? " "
         @SQL_MATCH = params[:search].split(/ /).join("|")
         @QUERY_SQL = "SELECT * FROM products WHERE name ~ '#@SQL_MATCH'"
-        #@QUERY_SQL = "SELECT * FROM products WHERE name ~ '(#@SQL_MATCH)'"
       else
         @QUERY_SQL = "SELECT * FROM products WHERE name ~ '#{ params[:search] }'"
       end
-      #@QUERY_SQL = "SELECT products.id,             products.image_url,
-      #                     products.price,          products.active,
-      #                     products.name,           products.thumb,
-      #                     products.pictures,       products.description,
-      #                     products.category_id
-      #              FROM products 
-      #              INNER JOIN categories
-      #              ON products.category_id = categories.id #              WHERE categories.name ~ '{ params[:search] }'
-      #              UNION
-      #              SELECT * FROM products WHERE name ~ '{ params[:seatch] }'"
       @products = Product.find_by_sql(@QUERY_SQL)
-    else
-      sleep(2.0)
-      redirect_to root_path
     end
     @order_item = current_order.order_items.new
   end
