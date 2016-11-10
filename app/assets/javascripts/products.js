3d671ed6fbc49b86ed1de786c0d3a05d6c1b0c86
dxwang <dxwang68@126.com>
2016-11-10 11:33:08 +0800

    fix cart

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,14 +2,13 @@
  ***********************
  */
 // Deleting items
-$('body').on("click", ".shopping-cart .delete a i", function(){
+$('*').on("click", ".shopping-cart .delete a i", function(){
   var $target = $(this).parent().parent().parent();
   var $positions = $('.shopping-cart .item');
   $target.hide(300, function(){
     $.when($.target.remove()).then( function() {
       if($positions.length === 1) {
         $(".shopping-cart .items-list").remove();
-        $(".shopping-cart .title").text("Shopping cart is empty!");
       }
     });
   });
