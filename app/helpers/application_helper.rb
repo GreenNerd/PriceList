4ca95821988f09f7349e2c716c7f8a02144653fe
dxwang <dxwang68@126.com>
2016-11-14 13:55:33 +0800

    correct image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,26 @@
 module ApplicationHelper
-  def qiniu_image_url(product, format = :raw)
-    url = "http://ogf4uc3pg.bkt.clouddn.com/#{product.image_url}"
+  def qiniu_image_url(name, format = :raw)
+    url = "http://ogf4uc3pg.bkt.clouddn.com/#{name}"
 
     case format
     when :index
       url << '?imageMogr2/thumbnail/173x170!'
+    when :sel
+      url << '?imageMogr2/thumbnail/200x200!'
+    when :carousel
+      url << '?imageMogr2/thumbnail/x300'
     when :raw
-      url << "?attname=#{product.image_url}"
+      url << "?attname=#{name}"
     end
+  end
+
+  def qiniu_carousel(images)
+    a = []
+    if images.count >= 1
+      images.each do |img|
+        a << qiniu_image_url(img, :carousel)
+      end
+    end
+    return a
   end
 end
