86c8e0813b4dfcdcb9f2a3141e5694d43be8f9b6
dxwang <dxwang68@126.com>
2016-11-15 16:51:26 +0800

    fix pdf generator

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,11 +5,12 @@ class CartsController < ApplicationController
 
   def genpdf
     @order_items = current_order.order_items
+    @order = current_order
 
     respond_to do |format|
       format.html
       format.pdf do
-        pdf = GenPdf.new(@order_items)
+        pdf = GenPdf.new(@order_items, @order)
         send_data pdf.render, filename: 'report.pdf', type: 'application/pdf', :disposition => 'inline'
       end
     end
--- app/pdfs/gen_pdf.rb
+++ app/pdfs/gen_pdf.rb
@@ -2,46 +2,43 @@ require 'prawn'
 require 'prawn/table'
 
 class GenPdf < Prawn::Document
-  def initialize(order_items)
+  def initialize(order_items, order)
     super()
     @items = order_items
+    @order = order
+    table_content
+    order_content
     header
     text_content
-    table_content
   end
 
   def header
     # This inserts an image in the pdf file and set the size of the image
+    move_down 20
     image "#{Rails.root}/app/assets/images/QR.png"
+    move_down 20
   end
 
   def text_content
-    # The cursor for inserting content starts on the top of the page. Here we move it down a little to create more space between the text and image inserted above
-    y_position = cursor - 30
-
-    # The bounding_box takes the x and y coordinates for positioning its content and some options to style it
-    bounding_box([0, y_position], :width => 270, :height => 300) do
-      font("#{Prawn::DATADIR}/fonts/msyh.ttf", :size => 12) do
-        text "存储互联二维码", :align => :center
-      end
+    font("#{Prawn::DATADIR}/fonts/simkai.ttf", :size => 12) do
+      text "存储互联二维码", :align => :center
     end
-
   end
 
   def table_content
-    # This makes a call to product_rows and gets back an array of data that will populate the columns and rows of a table
+    # This makes a call to item_rows and gets back an array of data that will populate the columns and rows of a table
     # I then included some styling to include a header and make its text bold. I made the row background colors alternate between grey and white
     # Then I set the table column widths
     table item_rows do
-      row(0).font_style = :bold
       self.header = true
       self.row_colors = ['DDDDDD', 'FFFFFF']
       self.column_widths = [100, 100, 100, 100, 100]
+      self.cell_style = {size: 12, :font => "#{Prawn::DATADIR}/fonts/simkai.ttf"}
     end
   end
 
   def item_rows
-    [['#', 'name', 'Unit Price', 'quantity', 'Total Price']] +
+    [['商品分类', '商品名称', '商品单价', '商品数量', '商品总价']] +
       @items.map do |item|
       if item.present?
         sid = item.stock_keeping_unit_id
@@ -49,8 +46,26 @@ class GenPdf < Prawn::Document
         pid = @sku.product_id
         @product = Product.find_by(id: pid)
         pname = @product.name
+        cid = @product.category_id
+        @category = Category.find_by(id: cid)
+        cname = @category.name
       end
-      [item.id, item.quantity, item.unit_price, item.quantity, item.total_price]
+      [pname, cname, item.unit_price, item.quantity, item.total_price]
     end
+  end
+
+  def order_content
+    move_down 10
+    table order_rows do
+      self.header = true
+      self.row_colors = ['DDDDDD', 'FFFFFF']
+      self.column_widths= [250, 250]
+      self.cell_style = {size: 13, :font => "#{Prawn::DATADIR}/fonts/simkai.ttf"}
+    end
+  end
+  
+  def order_rows
+    [['商品件数', +'价格合计']] +
+      [[@order.total, @order.subtotal]]
   end
 end
