5316c0759748dc97af7188dc70f5ee3e204aaa48
dxwang <dxwang68@126.com>
2016-11-21 11:15:54 +0800

    :smile:

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -6,7 +6,7 @@ $('*').on("click", ".shopping-cart .delete a i", function(){
   var $target = $(this).parent().parent().parent();
   var $positions = $('.shopping-cart .item');
   $target.hide(300, function(){
-    $.when($.target.remove()).then( function() {
+    $.when($target.remove()).then( function() {
       if($positions.length === 1) {
         $(".shopping-cart .items-list").remove();
       }
