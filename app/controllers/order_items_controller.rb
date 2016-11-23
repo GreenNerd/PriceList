69d5b32ff59ebfe90a95fbc8281ce077e457e2f3
dxwang <dxwang68@126.com>
2016-11-23 16:12:10 +0800

    making order_item refresh while doing on the same product type

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -12,21 +12,28 @@ class OrderItemsController < ApplicationController
     @order = current_order
     if params[:order_item][:stock_keeping_unit_id].present?
       sid = params[:order_item][:stock_keeping_unit_id]
-      #if @order.order_items.find_by(stock_keeping_unit_id: sid).present?
-      #  @order_item = @order.order_items.find_by(stock_keeping_unit_id: sid)
-      #  newQua = @order_item[:quantity].to_i + params[:order_item][:quantity].to_i
-      #  @order_item.update_attributes(:quantity, newQua)
-      #  @order_items = @order.order_items
-      #else
+      if @order.order_items.find_by(stock_keeping_unit_id: sid).present?
+        @order_item = @order.order_items.find_by(stock_keeping_unit_id: sid)
+        newQua = @order_item[:quantity].to_i + params[:order_item][:quantity].to_i
+        @order_item.update_attribute(:quantity, newQua)
+        @order_items = @order.order_items
+      else
         @order_item = @order.order_items.new(order_item_params)
-      #end
+      end
     elsif params[:order_item][:product_id].present?
       pid = params[:order_item][:product_id]
       quantity = params[:order_item][:quantity]
       @sku = StockKeepingUnit.find_by(product_id: pid)
-      @order_item = @order.order_items
-                          .new({:quantity => quantity,
-                                :stock_keeping_unit_id => @sku.id})
+      if @order.order_items.find_by(stock_keeping_unit_id: @sku.id).present?
+        @order_item = @order.order_items.find_by(stock_keeping_unit_id: @sku.id)
+        newQua = @order_item[:quantity].to_i + params[:order_item][:quantity].to_i
+        @order_item.update_attribute(:quantity, newQua)
+        @order_items = @order.order_items
+      else
+        @order_item = @order.order_items
+                            .new({:quantity => quantity,
+                                  :stock_keeping_unit_id => @sku.id})
+      end
     end
     @order.save
     session[:order_id] = @order.id
