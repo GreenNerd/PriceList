30c2d7d7ba207248ffbddc4ffd10463c725d64ad
lumuy <dxwang68@126.com>
2016-12-08 03:03:00 +0800

    add mutiple image success

--- README.md
+++ README.md
@@ -26,3 +26,5 @@ Things you may want to cover:
 * CREATE EXTENSION hstore WITH SCHEMA pg_catalog;
   psql -U dbuser -d shop_app
   # add hstore type extension support
+
+* select setval('stock_keeping_units_id_seq', (select max(id) from stock_keeping_units)+1);
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
+require 'carrierwave/orm/activerecord'
+
 class Product < ApplicationRecord
   has_many :stock_keeping_units, dependent: :destroy
   accepts_nested_attributes_for :stock_keeping_units, allow_destroy: true
@@ -8,5 +10,5 @@ class Product < ApplicationRecord
 
   default_scope { where(active: true) }
 
-  # mount_uploaders :image_url, ImageUploader
+  mount_uploaders :image_url, ImageUploader
 end
